7e48b89a0be67755f59781e32e21f4736cca0afe
devlopersabbir <devlopersabbir@gmail.com>
2024-04-15 02:58:40 +0600

    [feat Γëí╞Æ├ä├º] endless repeating background image Γëí╞Æ├┐├▒

--- scripts/background.js
+++ scripts/background.js
@@ -7,8 +7,11 @@ export class Background {
   constructor(game) {
     this.game = game;
     this.image = document.getElementById("background");
+    this.image2 = document.getElementById("background2");
     this.width = 2400;
     this.height = this.game.baseHeight;
+    this.scaleHeight;
+    this.scaleWidth;
     this.x;
   }
   update() {
@@ -16,9 +19,33 @@ export class Background {
     if (this.x <= -this.width) this.x = 0;
   }
   draw() {
-    this.game.ctx.drawImage(this.image, this.x, 0);
+    this.game.ctx.drawImage(
+      this.image,
+      this.x,
+      0,
+      this.scaleWidth,
+      this.scaleHeight
+    );
+    this.game.ctx.drawImage(
+      this.image,
+      this.x + this.scaleWidth - 2,
+      0,
+      this.scaleWidth,
+      this.scaleHeight
+    );
+    if (this.game.canvas.width >= this.scaleWidth) {
+      this.game.ctx.drawImage(
+        this.image,
+        this.x + this.scaleWidth * 2 - 2,
+        0,
+        this.scaleWidth,
+        this.scaleHeight
+      );
+    }
   }
   resize() {
+    this.scaleWidth = this.width * this.game.ratio;
+    this.scaleHeight = this.height * this.game.ratio;
     this.x = 0;
   }
 }
